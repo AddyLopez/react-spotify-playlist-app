75756661d33320b40c13607699fce05a12de0bf5
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-03-04 17:31:06 -0700

    Iteratively render list of tracks in Tracklist.js

--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./Tracklist.css";
 import Track from "../Track/Track.js";
 
-function Tracklist() {
+function Tracklist({ searchResults }) {
   return (
     <>
       <p>Hello, from Tracklist!</p>
-      <Track />
+      {searchResults.map((track) => {
+        return <Track key={track.id} track={track} />;
+      })}
     </>
   );
 }
