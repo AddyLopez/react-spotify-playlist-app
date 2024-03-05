a2125f78ef019a4281594e3828ab6b1c4d416327
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-03-04 17:27:57 -0700

    Pass searchResults props to Tracklist component

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./SearchResults.css";
 import Tracklist from "../Tracklist/Tracklist.js";
 
-function SearchResults() {
+function SearchResults({ searchResults }) {
   return (
     <section className="SearchResults">
       <h2>Results</h2>
-      <Tracklist />
+      <Tracklist searchResults={searchResults} />
     </section>
   );
 }
