a1a234dc413272af57e716b81a8af6428dcee965
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-03-04 17:18:26 -0700

    Set up conditional rendering in Track.js

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./Track.css";
 
-function Track() {
+function Track(props) {
+  const renderButton = () => {
+    if (props.isDelete) {
+      return <button className="track-action">-</button>;
+    } else {
+      return <button className="track-action">+</button>;
+    }
+  };
+
   return (
     <section className="Track">
       <h3>Track Name</h3>
       <p>
         <span>Artist</span>
         <span className="album">Album</span>
-        <span>
-          <button className="track-action">+/-</button>
-        </span>
+        <span>{renderButton()}</span>
       </p>
     </section>
   );
