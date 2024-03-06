82d36dd187987eb287e9573cec2b7189c6cb183a
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-03-05 17:35:37 -0700

    Pass props from Tracklist to Track

--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./Tracklist.css";
 import Track from "../Track/Track.js";
 
-function Tracklist({ searchResults }) {
+function Tracklist({ searchResults, onAdd, isDelete }) {
   return (
     <>
       <p>Hello, from Tracklist!</p>
       {searchResults.map((track) => {
-        return <Track key={track.id} track={track} />;
+        return <Track key={track.id} track={track} onAdd={onAdd} />;
       })}
     </>
   );
