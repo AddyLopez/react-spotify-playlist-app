2198f91d13e02297c544dde6c64e458c02929cd2
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-03-06 17:52:15 -0700

    Set up state and search event handler

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-function SearchBar() {
+function SearchBar({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const search = () => {
+    onSearch(searchTerm);
+  };
+
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
+    search();
+  };
   return (
     <>
       <FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
       <input
         type="text"
+        onChange={handleSearchTermChange}
         placeholder="Search for a song, album, or artist"
         aria-label="search button"
       />
